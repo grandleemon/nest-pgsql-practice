24fbdbc861ce4b7f350fce5cce376f7678da7801
Max <zeetreex@gmail.com>
2022-06-11 12:42:28 +0300

    creating user via postman and getting all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {CreateUserDto} from "./dto/create-user-dto";
 
-@Controller('users')
-export class UsersController {}
+@Controller('/users')
+export class UsersController {
+
+  constructor(private readonly usersService: UsersService) {
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.getAllUsers()
+  }
+
+  @Post()
+  createUser(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto)
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import {User} from "./user.model";
+import {Injectable} from "@nestjs/common";
+import {InjectModel} from "@nestjs/sequelize";
+import {CreateUserDto} from "./dto/create-user-dto";
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(@InjectModel(User) private readonly userRepository: typeof User) {
+  }
+
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.create(dto)
+    return user;
+  }
+
+  async getAllUsers() {
+    const users = await this.userRepository.findAll()
+    return users
+  }
+}
