efb23853aedc78eb5dc484e4b0647f9725299248
Max <zeetreex@gmail.com>
2022-06-11 15:25:02 +0300

    implemented the register and login functionality, hashing passwords using bcrypt and saving them in database, generating a JWT token if user logged in or registered

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import {HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
+import {CreateUserDto} from "../users/dto/create-user-dto";
+import {UsersService} from "../users/users.service";
+import {JwtService} from "@nestjs/jwt";
+import * as bcrypt from 'bcryptjs'
+import {User} from "../users/user.model";
+
+@Injectable()
+export class AuthService {
+
+  constructor(private readonly userService: UsersService, private readonly jwtService: JwtService) {
+
+  }
+
+
+  async login(dto: CreateUserDto) {
+    const user = await this.validateUser(dto)
+    return this.generateToken(user)
+  }
+
+  async register(dto: CreateUserDto) {
+    const candidate = await this.userService.getUserByEmail(dto.email)
+    if (candidate) {
+      throw new HttpException('User with this email already exists', HttpStatus.BAD_REQUEST)
+    }
+    const hashPassword = await bcrypt.hash(dto.password, 5)
+    const user = await this.userService.createUser({...dto, password: hashPassword})
+    return this.generateToken(user)
+  }
+
+  private async generateToken(user: User) {
+    const payload = {email: user.email, id: user.id, roles: user.roles}
+    return {
+      token: this.jwtService.sign(payload)
+    }
+  }
+
+  private async validateUser(dto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(dto.email)
+    const passwordEquals = await bcrypt.compare(dto.password, user.password);
+    if (user && passwordEquals) {
+      return user
+    }
+    throw new UnauthorizedException({message: "Incorrect data"})
+  }
+}
